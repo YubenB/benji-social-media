a7788d60d689a460b50155e517f982698c8f3daf
ajiesep <ajiesep@gmail.com>
2024-08-14 19:33:38 +0700

    create migration

---
+++ migrations/20240814121921-create-post.js
@@ -0,0 +1,41 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Posts", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      description: {
+        type: Sequelize.STRING,
+      },
+      imgUrl: {
+        type: Sequelize.STRING,
+      },
+      postDate: {
+        type: Sequelize.DATE,
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Posts");
+  },
+};
