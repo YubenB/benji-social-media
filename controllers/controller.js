ad8955ada04e1ae98c32cabe145d742588a2327b
ajiesep <ajiesep@gmail.com>
2024-08-15 10:50:55 +0700

    template controller

---
+++ controllers/controller.js
@@ -0,0 +1,55 @@
+const {} = require("../models");
+const { Op } = require("sequelize");
+
+class Controller {
+  static async(req, res) {
+    try {
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  static async(req, res) {
+    try {
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  static async(req, res) {
+    try {
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  static async(req, res) {
+    try {
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  static async(req, res) {
+    try {
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  static async(req, res) {
+    try {
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  static async(req, res) {
+    try {
+    } catch (error) {
+      res.send(error);
+    }
+  }
+}
+
+module.exports = Controller;
