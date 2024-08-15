292bbe152491dd6579fa9894c2543d18583f2ed9
yubenbauty <yubenbauty@gmail.com>
2024-08-15 17:02:05 +0700

    creating relation on model

--- models/profile.js
+++ models/profile.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Profile extends Model {
     /**
@@ -11,17 +9,31 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Profile.belongsTo(models.User);
     }
   }
-  Profile.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    bio: DataTypes.STRING,
-    private: DataTypes.BOOLEAN,
-    UserId: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Profile',
+  Profile.init(
+    {
+      firstName: DataTypes.STRING,
+      lastName: DataTypes.STRING,
+      bio: DataTypes.STRING,
+      profilePicture: DataTypes.STRING,
+      private: DataTypes.BOOLEAN,
+      UserId: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "Profile",
+    }
+  );
+
+  Profile.beforeCreate(async (profile, option) => {
+    if (!profile.firstName && !profile.lastName && !profile.profilePicture) {
+      profile.firstName = "Edit";
+      profile.lastName = "Your name";
+      profile.profilePicture =
+        "https://wallpapers-clan.com/wp-content/uploads/2022/08/default-pfp-18.jpg";
+    }
   });
   return Profile;
 };
