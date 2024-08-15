3ded4bed2d5070775681dc9d300ec144e4abcbeb
ajiesep <ajiesep@gmail.com>
2024-08-15 11:03:05 +0700

    login form

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,15 +2,17 @@ const {} = require("../models");
 const { Op } = require("sequelize");
 
 class Controller {
-  static async(req, res) {
+  static async home(req, res) {
     try {
+      res.render("home");
     } catch (error) {
       res.send(error);
     }
   }
 
-  static async(req, res) {
+  static async loginForm(req, res) {
     try {
+      res.render("loginForm");
     } catch (error) {
       res.send(error);
     }
